b0d272b142f7e9a42fde1a97c0ed112dd0768391
Mike Steinmetz <mikesteinmetz00@gmail.com>
2021-08-19 21:00:59 -0500

    updating get and post routes

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,22 +1,27 @@
 const router = require("express").Router();
 const userWorkout = require("../models/userWorkouts");
 const mongojs = require("mongojs");
-const { db } = require("../models/userWorkouts");
 
 router.get("/api/workouts", (req, res) => {
-    db.agggregate( {
-        $addFields: {
-            duration: {
-                $sum: "$exercises.duration"
+    userWorkout.find({})
+        .populate("exercises")
+        .exec(function (err, workouts) {
+            if (err) {
+                console.log(err);
+            } else {
+                res.json(workouts);
             }
-        }
-    })
-    .then(data => {
-        res.json(data);
+        })
+});
+
+router.post("/api/workouts", ( { body }, res) => {
+userWorkout.create(body)
+    .then(dbuserWorkouts => {
+        res.json(dbuserWorkouts);
     })
     .catch(err => {
         res.status(400).json(err);
-    })
+    });
 });
 
 module.exports = router;
