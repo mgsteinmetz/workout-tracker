40e53edc3e63681c9db313205e3bc38e4599f6dc
Mike Steinmetz <mikesteinmetz00@gmail.com>
2021-08-24 17:17:11 -0500

    adding sets, reps for model

--- models/userWorkouts.js
+++ models/userWorkouts.js
@@ -3,18 +3,33 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-    exercises: {
-        type: String,
-        trim: true
-    },
-    duration: {
-        type: Number,
-        default: 0
-    },
-    date: {
-        type: Date,
-        default: Date.now
-    }
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: 'Enter type of exercise'
+            },
+            duration: {
+                type: Number,
+                trim: true,
+                required: 'Enter duration of exercise'
+            },
+            date: {
+                type: Date,
+                default: Date.now
+            },
+            weight: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            }
+        }
+    ]
 });
 
 const userWorkout = mongoose.model("userWorkout", workoutSchema);
