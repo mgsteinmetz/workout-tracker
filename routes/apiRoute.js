c842b603eb378d6cdd087fcbd951256aefa5c149
Mike Steinmetz <mikesteinmetz00@gmail.com>
2021-08-24 16:47:50 -0500

    adding get, and put routes for ':id and range

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -24,4 +24,33 @@ userWorkout.create(body)
     });
 });
 
+router.put("/api/workouts/:id", async (req, res) => {
+    userWorkout.findOneAndUpdate(
+        { _id: mongojs.ObjectId(req.params.id) },
+        { duration: req.body.duration },
+        { new: true }
+    )
+    .then(dbuserWorkouts => {
+        res.json(dbuserWorkouts);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+    userWorkout.countDocument({}, function (err, range) {
+        userWorkout.find({})
+            .populate("exercises")
+            .skip(range > 7 ? range - 7 : 0)
+            .exec(function (err, workouts) {
+                if(err) {
+                    console.log(err);
+                } else {
+                    res.json(workouts)
+                }
+            })
+    });
+});
+
 module.exports = router;
